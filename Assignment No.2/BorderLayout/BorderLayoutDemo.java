8819504af01629803e11793adfd7360588830499
DeepakGtm123 <53470604+DeepakGtm123@users.noreply.github.com>
2020-03-02 18:12:03 +0545

    Update BorderLayoutDemo.java

--- Assignment No.2/BorderLayout/BorderLayoutDemo.java
+++ Assignment No.2/BorderLayout/BorderLayoutDemo.java
@@ -1,11 +1,29 @@
-import java.awt.*;
-class BorderLayoutDemo{
-  BorderLayoutDemo(){
-    Frame f = new Frame("Frame1");
-    f.setVisible(true);
-    f.setSize(500,400);
-    f.setLayout(new BorderLayout());
-    
+import java.awt.*;   
   
+public class BorderLayoutDemo {  
+  Frame f;  
+  BorderLayoutDemo(){  
+    f=new JFrame();  
+      
+    Button b1=new Button("NORTH");;  
+    Button b2=new Button("SOUTH");;  
+    Button b3=new Button("EAST");;  
+    Button b4=new Button("WEST");;  
+    Button b5=new Button("CENTER");;  
+      
+    f.add(b1,BorderLayout.NORTH);  
+    f.add(b2,BorderLayout.SOUTH);  
+    f.add(b3,BorderLayout.EAST);  
+    f.add(b4,BorderLayout.WEST);  
+    f.add(b5,BorderLayout.CENTER);  
+      
+    f.setSize(300,300);  
+    f.setVisible(true);  
+  } 
+  
+  public static void main(String[] args) {  
+    new BorderLayoutDemo();  
   }
-}
+  
+} 
+
