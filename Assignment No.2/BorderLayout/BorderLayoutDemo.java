ef6f408c5580bf11fcdce9e6ceab165e3f29b4a7
DeepakGtm123 <53470604+DeepakGtm123@users.noreply.github.com>
2020-03-02 16:40:08 +0545

    Create BorderLayoutDemo.java

---
+++ Assignment No.2/BorderLayout/BorderLayoutDemo.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+class BorderLayoutDemo{
+  BorderLayoutDemo(){
+    Frame f = new Frame("Frame1");
+    f.setVisible(true);
+    f.setSize(500,400);
+    f.setLayout(new BorderLayout());
+    
+  
+  }
+}
